0938777d8174989316e09d4f72954816034c33ec
wimmerm <git-test@wiso.de>
2018-08-15 01:57:32 +0200

    test alert, button press

--- js/main.js
+++ js/main.js
@@ -64,6 +64,7 @@ function initCameraUI() {
     // https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/ARIA_Techniques/Using_the_button_role
 
     takePhotoButton.addEventListener("click", function() {
+        alert("press");
         takeSnapshotUI();
         takeSnapshot();        
     });
